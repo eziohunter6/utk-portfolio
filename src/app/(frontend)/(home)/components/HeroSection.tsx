27986b3faf0aa8324e921bc82ca683a7310890b3
Drish-xD <drish.xd@gmail.com>
2026-03-07 02:08:31 +0530

    chore: new hero section

--- src/app/(frontend)/(home)/components/HeroSection.tsx
+++ src/app/(frontend)/(home)/components/HeroSection.tsx
@@ -1,51 +1,82 @@
 import { RichText } from "@payloadcms/richtext-lexical/react";
-import Image from "next/image";
+import Image, { getImageProps } from "next/image";
 import Section from "@/components/ui/Section";
 import { filterPopulated } from "@/lib/utils";
 import type { Home } from "@/payload-types";
 
 type Props = NonNullable<Home["hero"]>;
 
-const HeroSection = ({ title, subtitle, content, workedAt }: Props) => (
-  <Section id="hero">
-    <div className="flex flex-col justify-center gap-8 mb-16 pt-32 sm:pt-40">
-      <h1 className="text-5xl sm:text-6xl md:text-8xl lg:text-[10rem]/45 font-semibold">
-        {title}
-      </h1>
+const HeroSection = ({ title, subtitle, content, workedAt }: Props) => {
+  const {
+    props: { srcSet: mobile },
+  } = getImageProps({
+    alt: "Hero",
+    src: "/images/home/hero-mobile.webp",
+    sizes: "100vw",
+    preload: true,
+    height: 600,
+    width: 800,
+  });
 
-      <h2 className="text-3xl sm:text-[3rem]/15 md:text-6xl lg:text-[5rem]/24 text-right whitespace-nowrap py-8 border-y-2 border-current">
-        {subtitle}
-      </h2>
-    </div>
+  const {
+    props: { srcSet: desktop, ...rest },
+  } = getImageProps({
+    alt: "Hero",
+    src: "/images/home/hero-desktop.webp",
+    sizes: "100vw",
+    preload: true,
+    height: 1080,
+    width: 1920,
+  });
 
-    <div className="w-full md:w-3/4 lg:w-3/5 my-16">
-      <RichText data={content} className="prose" />
-    </div>
+  return (
+    <Section id="hero">
+      <div className="flex flex-col justify-center gap-8 mb-16 pt-8">
+        <h1 className="text-5xl sm:text-6xl md:text-8xl lg:text-[10rem]/45 font-semibold border-b-2 border-current pb-4">
+          {title}
+        </h1>
 
-    {/* Worked At */}
-    <h3 className="text-sm font-semibold text-center">WORKED AT</h3>
+        <h2 className="text-3xl sm:text-[3.5rem] md:text-[3.5rem] lg:text-[4rem] text-right whitespace-nowrap">
+          {subtitle}
+        </h2>
 
-    {/* TODO: add marqure text here */}
-    <div className="flex flex-row justify-center py-4 border-b-2 border-current">
-      {filterPopulated(workedAt).map(({ alt, url, filename }) => {
-        if (!url) return null;
+        <div className="relative left-1/2 right-1/2 -mx-[50vw] w-screen -mt-[10%] md:-mt-[15%] -mb-[20%] md:-mb-[25%]">
+          <picture>
+            <source media="(max-width: 640px)" srcSet={mobile} />
+            <img {...rest} srcSet={desktop} className="object-cover" />
+          </picture>
+        </div>
+      </div>
 
-        return (
-          <div key={filename} className="flex flex-row items-center group">
-            <Image
-              src={url}
-              alt={alt ?? filename ?? ""}
-              height={32}
-              width={100}
-              className="object-contain h-8 w-auto"
-            />
+      <div className="w-full md:w-3/4 lg:w-3/5 my-16">
+        <RichText data={content} className="prose" />
+      </div>
 
-            <span className="shrink-0 mx-2 sm:mx-4 h-2/3 w-[1.5px] rounded-full bg-[#EDF0FF] group-last:hidden" />
-          </div>
-        );
-      })}
-    </div>
-  </Section>
-);
+      {/* Worked At */}
+      <h3 className="text-sm font-semibold text-center">WORKED AT</h3>
+
+      {/* TODO: add marqure text here */}
+      <div className="flex flex-row justify-center py-4 border-b-2 border-current">
+        {filterPopulated(workedAt).map(({ alt, url, filename }) => {
+          if (!url) return null;
+
+          return (
+            <div key={filename} className="flex flex-row items-center group">
+              <Image
+                src={url}
+                alt={alt ?? filename ?? ""}
+                height={32}
+                width={100}
+                className="object-contain h-8 w-auto"
+              />
+
+              <span className="shrink-0 mx-2 sm:mx-4 h-2/3 w-[1.5px] rounded-full bg-[#EDF0FF] group-last:hidden" />
+            </div>
+          );
+        })}
+      </div>
+    </Section>
+  );
+};
 
 export default HeroSection;
