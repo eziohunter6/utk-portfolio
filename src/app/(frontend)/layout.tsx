ad20763a3c0f74803780bb71d387cfbed9e80680
Drish-xD <drish.xd@gmail.com>
2026-03-10 01:09:22 +0530

    feat: add @vercel/analytics package and integrate Analytics component in frontend layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import SmoothScroll from "@/components/LenisProvider";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
@@ -63,6 +64,7 @@ export default function FrontendLayout({
           {modal}
           <Footer />
         </SmoothScroll>
+        <Analytics />
       </body>
     </html>
   );
